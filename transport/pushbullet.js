f789b8589fb0349e5b219fda7efdbded9ba7ce2a
Kamil Wojtczyk <k.wojtczyk@gmail.com>
2017-04-04 10:40:11 +0100

    Quick bugfix if no deviceId parameter is provided

--- transport/pushbullet.js
+++ transport/pushbullet.js
@@ -6,13 +6,13 @@ const pushbullet = {
   sendMessage
 }
 
-function sendMessage (deviceId = null, title, body, url, callback) {
+function sendMessage (deviceId, title, body, url, callback) {
   let options = {
     method: 'POST',
     uri: 'https://api.pushbullet.com/v2/pushes',
     form: {
       type: 'link',
-      device_iden: deviceId,
+      device_iden: deviceId || '',
       title,
       body,
       url
